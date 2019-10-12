312b4bd7a491a8c63bc1cb49ff88cde9781eb0ae
hisan <13821823961@163.COM>
2019-10-12 18:01:37 +0800

    info

--- sh_dir/consistent_rewind.sh
+++ sh_dir/consistent_rewind.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-exec 1>testout
-echo "this is an test 1"
-echo "this is an test 2"
-echo "this is an test 3"
+
+exec 6<&0
+exec 0< testfile
+
+count=1
+while read line
+do 
+	echo "Line #$count:$line"
+	count=$[ $count + 1]
+done 
+exec 0<&6
+read -p "Are yout done now? " answer
+case "$answer" in
+	Y | y) echo "Goodbye";;
+	N | y) echo "Sorry ,this is the end"
+esac
