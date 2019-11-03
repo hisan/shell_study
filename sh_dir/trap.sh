54fd205487ee4cf1f5424972a77ba3a0f284d79c
hisan <13821823961@163.COM>
2019-11-03 21:14:32 +0800

    info

--- sh_dir/trap.sh
+++ sh_dir/trap.sh
@@ -7,7 +7,7 @@ while [ $count -le 4 ]
 do 
 	echo "Loop #$count"
 	sleep 1
-	count=$[ $count + 1]
+	count=$((count + 1))
 done 
 echo "end"
 #remove signal SIGINT
@@ -17,5 +17,5 @@ while [ $count -le 10 ]
 do 
 	echo "Loop #$count"
 	sleep 1
-	count=$[ $count + 1]
+	count=$((count + 1))
 done 
